ca2bd725c6e77e2757e5e467d3571cfbe985f153
Lucas Almeida <lucasaf@dcomp.ufs.br>
2020-07-06 19:14:49 -0300

    Adição da nova Bottom Navigation Bar.

--- lib/icons/custom_icons_icons.dart
+++ lib/icons/custom_icons_icons.dart
@@ -32,11 +32,14 @@ class CustomIcons {
   static const IconData magnifier_with_an_eye2 = IconData(0xe809, fontFamily: _kFontFam);
   static const IconData notification = IconData(0xe80a, fontFamily: _kFontFam);
   static const IconData password = IconData(0xe80b, fontFamily: _kFontFam);
-  static const IconData qr_code = IconData(0xe80c, fontFamily: _kFontFam);
+  static const IconData ticket_magnifier = IconData(0xe80c, fontFamily: _kFontFam);
   static const IconData running_repair_man_with_wrench_and_kit = IconData(0xe80d, fontFamily: _kFontFam);
   static const IconData settings__1_ = IconData(0xe80e, fontFamily: _kFontFam);
   static const IconData user = IconData(0xe80f, fontFamily: _kFontFam);
   static const IconData user_image_with_black_background = IconData(0xe810, fontFamily: _kFontFam);
   static const IconData thumb_up = IconData(0xe811, fontFamily: _kFontFam);
   static const IconData thumb_down = IconData(0xe812, fontFamily: _kFontFam);
+  static const IconData add = IconData(0xe813, fontFamily: _kFontFam);
+  static const IconData search = IconData(0xe814, fontFamily: _kFontFam);
+  static const IconData qr_code = IconData(0xe815, fontFamily: _kFontFam);
 }
