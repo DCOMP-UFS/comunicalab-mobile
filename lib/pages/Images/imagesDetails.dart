ef379ef43ccd49d6192d6dc3c95ccfcbb9a2e320
paulo-zx <paulo-zx@hotmail.com>
2020-07-26 00:10:29 -0300

    atualização

--- lib/pages/Images/imagesDetails.dart
+++ lib/pages/Images/imagesDetails.dart
@@ -1,6 +1,7 @@
 import 'package:comunica_mobile/pages/Images/infoTela.dart';
 import 'package:comunica_mobile/pages/Images/softwareTela.dart';
 import 'package:flutter/material.dart';
+import 'package:comunica_mobile/widgets/sideBar.dart';
 
 
 class imagesDetails extends StatefulWidget {
@@ -12,81 +13,73 @@ class imagesDetails extends StatefulWidget {
   _imagesDetailsState createState() => _imagesDetailsState();
 }
 
-class _imagesDetailsState extends State<imagesDetails> with SingleTickerProviderStateMixin {
 
-  TabController _tabController;
+class _imagesDetailsState extends State<imagesDetails> {
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
+  Widget customTab(String label) {
+    return Tab(
+      child: Container(
+        width: 117,
+        height: 30,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          border: Border.all(color: Colors.white, width: 2),
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            label,
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
+    );
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF000080),
-        title: Text(widget.softwareName),
-        elevation: 0,
-        bottom: TabBar(
-          unselectedLabelColor: Colors.white,
-          labelColor: Color(0xFF000080),
-          indicatorColor: Colors.white,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorPadding: EdgeInsets.all(10),
-          indicator: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              color: Colors.white,
-          ),
-          controller: _tabController,
-          tabs: <Widget>[
-            Tab(
+
+      return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.softwareName),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(50.0),
               child: Container(
-                width: 120,
-                height: 25,
-                decoration: BoxDecoration(
+                height: 30,
+                margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 7.0),
+                child: TabBar(
+                  onTap: (tab) {
+
+                  },
+                  labelColor: Color(0xFF000080),
+                  unselectedLabelColor: Colors.white,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
-                    border: Border.all(
-                        color: Colors.white,
-                        width: 1
-                    )
-                ),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text("Informações"),
-                ),
-              ),
-            ),Tab(
-              child: Container(
-                width: 120,
-                height: 25,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(
-                        color: Colors.white,
-                        width: 1
-                    )
-                ),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text("Softwares"),
+                    color: Colors.white,
+                  ),
+                  tabs: [
+                    customTab("Informações"),
+                    customTab("Softwares"),
+                  ],
                 ),
               ),
             ),
-
-          ],
+          ),
+          drawer: handlerSideBar(context),
+          body: Container(
+            margin: EdgeInsets.all(0.0),
+            child: TabBarView(
+              children: <Widget>[
+                infoTela(),
+                softwareTela()
+              ],
+            ),
+          ),
+          //bottomNavigationBar: CustomBottomNavigationBar(),
         ),
-      ) ,
-      body: TabBarView(
-        controller: _tabController,
-        children: <Widget>[
-          infoTela(),
-          softwareTela()
-        ],
-      ),
-    );
+      );
   }
 }
