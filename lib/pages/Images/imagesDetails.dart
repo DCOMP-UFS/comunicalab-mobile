9d6f66cb6428c57a2d806c4e143a555bb01ea5cc
paulo-zx <paulo-zx@hotmail.com>
2020-07-26 00:13:51 -0300

    atualização

--- lib/pages/Images/imagesDetails.dart
+++ lib/pages/Images/imagesDetails.dart
@@ -78,7 +78,6 @@ class _imagesDetailsState extends State<imagesDetails> {
               ],
             ),
           ),
-          //bottomNavigationBar: CustomBottomNavigationBar(),
         ),
       );
   }
