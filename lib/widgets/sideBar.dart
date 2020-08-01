c1243bc9b23e98b9acd8d8d6eca3e894b39f5fa2
ivissonijs <ivisson.silva@dcomp.ufs.br>
2020-08-01 15:54:54 -0300

    create class addLabImage.dart

--- lib/widgets/sideBar.dart
+++ lib/widgets/sideBar.dart
@@ -1,3 +1,4 @@
+import 'package:comunica_mobile/pages/Laboratory/addLabImage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:comunica_mobile/icons/custom_icons_icons.dart';
@@ -245,7 +246,12 @@ Widget handlerSideBar(BuildContext context) {
                       color: Color(0xFF6A5ACD),
                     ),
                   ),
-                  onTap: () {
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context){
+                        return AddLabImage();
+                      }
+                    ));
                     // Navigator.of(context).push(MaterialPageRoute(
                     //     builder: (BuildContext context) =>
                     //         LabList()));
