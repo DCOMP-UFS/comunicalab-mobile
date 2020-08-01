68e6c915bd6f32068ee365bb157dc3500b0f1acc
ivissonijs <ivisson.silva@dcomp.ufs.br>
2020-08-01 16:03:04 -0300

    create class addLabImage_bloc.dart

---
+++ lib/pages/Laboratory/bloc/addLabImage_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+
+class AddLabImage_Bloc extends Bloc{
+  @override
+  // TODO: implement initialState
+  get initialState => throw UnimplementedError();
+
+  @override
+  Stream mapEventToState(event) {
+    // TODO: implement mapEventToState
+    throw UnimplementedError();
+  }
+  
+}
