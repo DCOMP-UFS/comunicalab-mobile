361599e20f4bd41ac5b707332bd2033c5068e5a9
paulo-zx <paulo-zx@hotmail.com>
2020-08-14 14:05:23 -0300

    atualização

--- lib/pages/Images/imagesList.dart
+++ lib/pages/Images/imagesList.dart
@@ -1,9 +1,8 @@
 import 'package:comunica_mobile/pages/Images/imagesDetails.dart';
-import 'package:comunica_mobile/pages/Images/searchTela.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:comunica_mobile/widgets/sideBar.dart';
 import 'package:comunica_mobile/pages/Images/widgets/customCard.dart';
-
 
 class ImagesList extends StatefulWidget {
   @override
@@ -11,35 +10,64 @@ class ImagesList extends StatefulWidget {
 }
 
 class _ImagesListState extends State<ImagesList> {
-
   //final itens = List<String>.generate(8, (index) => null);
 
-  @override
+  // var title = 'Lista de Imagens';
+  Widget appbarTitle = Text("Lista de Imagens");
+  var searchList = List<String>.generate(8, (index) => "Imagem ${index + 1}");
+  var copyList = List<String>.generate(8, (index) => "Imagem ${index + 1}");
+
+
+  final TextEditingController _controller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    print(searchList);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Lista de Imagens"),
+        title:  appbarTitle,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context)=>searchTela()
-              ));
+            onPressed: () {
+              searchList=copyList;
+              _controller.clear();
+              setState(() {
+                // title = '';
+                appbarTitle = Container(
+                  height: 30,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF9FDFF),
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: _controller,
+                    //controller: _textEditingController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: "Digite o nome da imagem",
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                    onSubmitted: (text) {
+                      setState(() {
+                        searchList = searchList.where((element) => element == text).toList();
+                      });
+                    },
+                  ),
+                );
+              });
             },
           )
         ],
       ),
       drawer: handlerSideBar(context),
-      body: Container(
+      body:  Container(
         margin: EdgeInsets.all(20.0),
         child: ListView.builder(
-          itemCount: 8,
+          itemCount: searchList.length,
           itemBuilder: (context, index) {
-            return  CustomCard('Imagem ${index + 1}');
-
+            return CustomCard(searchList[index]);
           },
         ),
       ),
@@ -47,3 +75,4 @@ class _ImagesListState extends State<ImagesList> {
   }
 
 }
+
