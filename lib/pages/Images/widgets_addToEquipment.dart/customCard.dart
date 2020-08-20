7c2a1ab7a34f1a59bae19b6c03b53619353bfe98
Renato Vinicius <renato.vini@outlook.com>
2020-08-20 19:32:03 -0300

    Adição da responsividade

--- lib/pages/Images/widgets_addToEquipment.dart/customCard.dart
+++ lib/pages/Images/widgets_addToEquipment.dart/customCard.dart
@@ -22,46 +22,45 @@ class _CustomCardState extends State<CustomCard> {
     final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
 
     final screenWidth = mediaQuery.width * devicePixelRatio;
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.9,
-      child: Card(
-        elevation: 5.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(10.0),
-          onTap: () {},
-          child: Container(
-              padding: EdgeInsets.all(1.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Checkbox(
-                    onChanged: (isMarked) {
-                      widget.equipment.isMarked = isMarked;
-                      BlocProvider.of<AddToEquipmentBloc>(context).add(EquipmentSelected(equipment: widget.equipment));
-                    },
-                    value: widget.equipment.isMarked,
-                    activeColor: Color(0xFF000080),
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SizedBox(height: mediaQuery.height * 0.015),
-                        Text(
-                          widget.equipment.name,
-                          style: TextStyle(),
-                        ),
-                        SizedBox(height: mediaQuery.height * 0.005),
-                        Text("Ar Condicionado"),
-                        SizedBox(height: mediaQuery.height * 0.02),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Container(
-                              width: screenWidth > 700 ? mediaQuery.width * 0.22 : mediaQuery.width * 0.28,
+    return Card(
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10.0),
+        onTap: () {},
+        child: Container(
+            padding: EdgeInsets.all(1.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Checkbox(
+                  onChanged: (isMarked) {
+                    widget.equipment.isMarked = isMarked;
+                    BlocProvider.of<AddToEquipmentBloc>(context).add(EquipmentSelected(equipment: widget.equipment));
+                  },
+                  value: widget.equipment.isMarked,
+                  activeColor: Color(0xFF000080),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: mediaQuery.height * 0.015),
+                      Text(
+                        widget.equipment.name,
+                        style: TextStyle(),
+                      ),
+                      SizedBox(height: mediaQuery.height * 0.005),
+                      Text("Ar Condicionado"),
+                      SizedBox(height: mediaQuery.height * 0.02),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Flexible(
+                            child: Container(
+                              // width: screenWidth > 700 ? mediaQuery.width * 0.22 : mediaQuery.width * 0.28,
                               child: Row(
                                 children: <Widget>[
                                   CircleAvatar(
@@ -75,8 +74,10 @@ class _CustomCardState extends State<CustomCard> {
                                 ],
                               ),
                             ),
-                            Container(
-                              width: screenWidth > 700 ? mediaQuery.width * 0.25 : mediaQuery.width * 0.33,
+                          ),
+                          Flexible(
+                            child: Container(
+                              // width: screenWidth > 700 ? mediaQuery.width * 0.25 : mediaQuery.width * 0.33,
                               child: Row(
                                 children: <Widget>[
                                   CircleAvatar(
@@ -90,16 +91,16 @@ class _CustomCardState extends State<CustomCard> {
                                 ],
                               ),
                             ),
-                            SizedBox(width: screenWidth > 700 ? mediaQuery.width * 0.1 : 0)
-                          ],
-                        ),
-                        SizedBox(height: mediaQuery.height * 0.015),
-                      ],
-                    ),
+                          ),
+                          // SizedBox(width: screenWidth > 700 ? mediaQuery.width * 0.1 : 0)
+                        ],
+                      ),
+                      SizedBox(height: mediaQuery.height * 0.015),
+                    ],
                   ),
-                ],
-              )),
-        ),
+                ),
+              ],
+            )),
       ),
     );
   }
