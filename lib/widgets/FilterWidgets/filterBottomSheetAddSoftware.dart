e9d6fe1a215f475099cebcfca7e613d25c3de901
ivissonijs <ivisson.silva@dcomp.ufs.br>
2020-09-03 09:46:21 -0300

    finalizando bottomSheet

--- lib/widgets/FilterWidgets/filterBottomSheetAddSoftware.dart
+++ lib/widgets/FilterWidgets/filterBottomSheetAddSoftware.dart
@@ -73,17 +73,39 @@ void modalBottomSheet(context){
                   Padding(
                     padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        TextField(
-                          autofocus: false,
-                          style: TextStyle(color: Colors.black),
-                          decoration: new InputDecoration(
-                            hintText: "Digite o nome do equipamento",
-                            enabledBorder: const OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.black26, width: 0.0),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          height: 45,
+                          alignment: Alignment.centerLeft,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            border: Border.all(
+                              color: Color(0xFFE0DFDF),
+                              width: 2,
                             ),
-                            border: const OutlineInputBorder(),
+                          ),
+                          child: Theme(
+                            data: new ThemeData(
+                              primaryColor: Colors.transparent,
+                              primaryColorDark: Colors.transparent,
+                            ),
+                            child: TextField(
+                              autofocus: true,
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  color: Colors.black45,
+                                  fontSize: 16.0,
+                                height: 1.5
+                              ),
+                              decoration: new InputDecoration(
+                                hintText: "Digite o nome do equipamento",
+                               enabledBorder: new OutlineInputBorder(
+                                 borderSide: BorderSide(color: Colors.transparent, ),
+                               ),
+                              ),
+                            ),
                           ),
                         ),
                         Padding(
@@ -100,9 +122,55 @@ void modalBottomSheet(context){
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
+
                           ),
                         ),
-                        
+                        InkWell(
+                          splashFactory: InkRipple.splashFactory,
+                          borderRadius: BorderRadius.circular(8.0),
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                            height: 45,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              border: Border.all(
+                                color: Color(0xFFE0DFDF),
+                                width: 2,
+                              ),
+                            ),
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<String>(
+                                icon: Icon(Icons.keyboard_arrow_down),
+                              //  value: filter["troubleType"],
+                                hint: Text(
+                                  'Escolha um tipo de problema',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black45,
+                                  ),
+                                ),
+                                isExpanded: true,
+                                onChanged: (String newValue) {
+                                 // updatePreferences(() {
+                                 //   filter["troubleType"] = newValue;
+                                 // });
+                                },
+                                items: <String>[
+                                  'Tipo de Problema 1',
+                                  'Tipo de Problema 2',
+                                  'Tipo de Problema 3',
+                                  'Tipo de Problema 4'
+                                ].map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -110,7 +178,6 @@ void modalBottomSheet(context){
               ),
             ),
           ),
-
         );
       }
   );
