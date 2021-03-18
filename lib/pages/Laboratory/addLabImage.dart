1b4b82cb0e2b897425d314647414e16974fe4421
Lucas Desktop <luycaslima@gmail.com>
2021-03-18 17:04:22 -0300

    Atualização do layout da lista de laboratório e correções
    Atualização do layout e correções e restruturação do seu Bloc para funcionar com o filtro de laboratórios.

--- lib/pages/Laboratory/addLabImage.dart
+++ lib/pages/Laboratory/addLabImage.dart
@@ -6,27 +6,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'addLabImage_List.dart';
 import 'bloc/addLabImage_bloc.dart';
 
-
 class AddLabImage extends StatelessWidget {
+  Ticket _ticket1 = new Ticket(status: "Livre");
+  Ticket _ticket2 = new Ticket(status: "Reservado");
+  Ticket _ticket3 = new Ticket(status: "Ocupado");
 
-   Ticket _ticket1 = new Ticket(status: "Livre");
-   Ticket _ticket2 = new Ticket(status: "Reservado");
-   Ticket _ticket3 = new Ticket(status: "Ocupado");
-
-
-  showAlertDialog1(BuildContext context)
-  {
+  showAlertDialog1(BuildContext context) {
     // configura o  AlertDialog
     AlertDialog alerta = AlertDialog(
       shape: new RoundedRectangleBorder(
-          borderRadius: new BorderRadius.circular(15)
-      ),
+          borderRadius: new BorderRadius.circular(15)),
       elevation: 100,
-      title: Text(
-          "Seu pedido foi solicitado!",
-          style: TextStyle(fontSize: 20, ),
-          textAlign: TextAlign.center
-      ),
+      title: Text("Seu pedido foi solicitado!",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+          textAlign: TextAlign.center),
     );
     // exibe o dialog
     showDialog(
@@ -39,12 +34,20 @@ class AddLabImage extends StatelessWidget {
 
   showAlertDialog2(BuildContext context) {
     Widget cancelaButton = FlatButton(
-      child: Text("Cancelar", style: TextStyle(fontSize: 20, color: Colors.black54),),
-      onPressed:  () {Navigator.of(context).pop();},
+      child: Text(
+        "Cancelar",
+        style: TextStyle(fontSize: 20, color: Colors.black54),
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
     );
     Widget continuaButton = FlatButton(
-      child: Text("OK", style: TextStyle(fontSize: 20, color: Colors.black54),),
-      onPressed:  () {
+      child: Text(
+        "OK",
+        style: TextStyle(fontSize: 20, color: Colors.black54),
+      ),
+      onPressed: () {
         Navigator.of(context).pop();
         showAlertDialog1(context);
       },
@@ -57,8 +60,7 @@ class AddLabImage extends StatelessWidget {
         textAlign: TextAlign.center,
       ),
       shape: new RoundedRectangleBorder(
-          borderRadius: new BorderRadius.circular(15)
-      ),
+          borderRadius: new BorderRadius.circular(15)),
       actions: [
         cancelaButton,
         continuaButton,
@@ -73,25 +75,28 @@ class AddLabImage extends StatelessWidget {
     );
   }
 
-
-
+//TODO o filtro nessa tela não está sendo aplicado
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context).devicePixelRatio * MediaQuery.of(context).size.width;
+    final mediaQuery = MediaQuery.of(context).devicePixelRatio *
+        MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
-          title:Column(
+          title: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text("Adicionar à Laboratórios"),
-              Text("Imagem 1", style: TextStyle(fontSize: 10),),
+              Text(
+                "Imagem 1",
+                style: TextStyle(fontSize: 10),
+              ),
             ],
           ),
           actions: [
             IconButton(
               icon: Icon(Icons.filter_list),
               onPressed: () {
-                FilterBottomSheetAddLaboratory().build(context);
+                //FilterBottomSheetAddLaboratory().build(context);
               },
             )
           ],
@@ -99,47 +104,52 @@ class AddLabImage extends StatelessWidget {
         bottomNavigationBar: Card(
           elevation: 8,
           child: Container(
-
-            padding: (mediaQuery > 700)? EdgeInsets.fromLTRB(90, 10, 90, 10) : EdgeInsets.fromLTRB(30, 10, 30, 10),
+            padding: (mediaQuery > 700)
+                ? EdgeInsets.fromLTRB(90, 10, 90, 10)
+                : EdgeInsets.fromLTRB(30, 10, 30, 10),
             height: 60,
             width: 360,
             child: BlocBuilder<AddLabImage_Bloc, int>(
-              builder: (context, state){
-                return  RaisedButton(
-                    onPressed: state > 0 ? () {
-                      showAlertDialog2(context);
-                    }: null,
-                    color: Color(0xFF000080),
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0)),
-                    child: Text(
-                      "Adicionar",
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                    padding: EdgeInsets.all(0),
-                  );
+              builder: (context, state) {
+                return RaisedButton(
+                  onPressed: state > 0
+                      ? () {
+                          showAlertDialog2(context);
+                        }
+                      : null,
+                  color: Color(0xFF000080),
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)),
+                  child: Text(
+                    "Adicionar",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                  padding: EdgeInsets.all(0),
+                );
               },
             ),
           ),
         ),
         body: SingleChildScrollView(
             child: Center(
-              child: Padding(
-                padding: EdgeInsets.only(top: 10, left: 3, right: 3),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    TicketCardLaboratoryImage(_ticket1, "Laboratório de Hardware", "Departamento"),
-                    TicketCardLaboratoryImage(_ticket1, "Laboratório de Hardware 2", "Departamento"),
-                    TicketCardLaboratoryImage(_ticket2, "Laboratório de Hardware", "Departamento"),
-                    TicketCardLaboratoryImage(_ticket2, "Laboratório de Hardware", "Departamento"),
-                    TicketCardLaboratoryImage(_ticket3, "Laboratório de Hardware", "Departamento"),
-
-                  ],
-                ),
-              ),
-            )
-        )
-    );
+          child: Padding(
+            padding: EdgeInsets.only(top: 10, left: 3, right: 3),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TicketCardLaboratoryImage(
+                    _ticket1, "Laboratório de Hardware", "Departamento"),
+                TicketCardLaboratoryImage(
+                    _ticket1, "Laboratório de Hardware 2", "Departamento"),
+                TicketCardLaboratoryImage(
+                    _ticket2, "Laboratório de Hardware", "Departamento"),
+                TicketCardLaboratoryImage(
+                    _ticket2, "Laboratório de Hardware", "Departamento"),
+                TicketCardLaboratoryImage(
+                    _ticket3, "Laboratório de Hardware", "Departamento"),
+              ],
+            ),
+          ),
+        )));
   }
 }
